e290f67dfc438b10f18c91be41d6b13d8e165cf1
狸猫科技 <zhaogangbjm@gmail.com>
2024-11-07 21:08:26 +0800

    Update route.ts

--- src/app/api/tags/route.ts
+++ src/app/api/tags/route.ts
@@ -1,6 +1,5 @@
 export async function GET(req: Request) {
-  const res = await fetch(
-    process.env.OLLAMA_URL + "/api/tags"
-  );
+  const OLLAMA_URL = 'http://localhost:11434';
+  const res = await fetch(OLLAMA_URL + "/api/tags");
   return new Response(res.body, res);
 }
